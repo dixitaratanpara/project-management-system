248023d1aae7958090a00c24a349eac0b92589dc
dixitaratanpara <dixita.ratanpara12@gmail.com>
2026-09-08 10:36:12 +0530

    Fix Render port binding

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 //connecte database
 connectDB();
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "127.0.0.1", () => {
+   console.log(`server is running on port ${PORT}`);
 });
 
